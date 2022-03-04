aefccbb729b788dab8aed0b68f2776d9645aa57b
Jack Delaney <bugjd@users.noreply.github.com>
2022-03-04 20:30:19 +0000

    Update scheduledTask.py

--- scheduledTask.py
+++ scheduledTask.py
@@ -5,7 +5,8 @@ import datetime
 from datetime import timedelta
 
 theCurrentTime = datetime.datetime.now()  # gets current time
-communication = communication("hello@example.net", "hello")
+communicationObject = communication.Communication("hello@example.net", "hello", "http://127.0.0.1:5000/")
+
 
 with open('members.json') as old_json_file:
     json_dict = json.load(old_json_file)  # opens the json file and converts it into a dictonary for python.
@@ -14,8 +15,8 @@ for people in json_dict:
     if theCurrentTime > (
             datetime.datetime.strptime((people["date"]), "%d/%m/%Y, %H:%M:%S") + timedelta(hours=people["hourstodo"])):
         # line above ^ once a certain amount people["hourstodo"] of hours has passed since the date was inputted
-        communication.sendEmail([people["email"]], people["name"], people["item"],
-                                people["uuid"])  # Conditon has been met for email notfication to be sent.
+       communicationObject.send_email([people["email"]], people["name"], people["item"],
+                                       people["uuid"])  # Conditon has been met for email notfication to be sent.
         print(people["date"], " with ", people["hourstodo"])
     else:
         print("not sent ", people["date"], " with ", people["hourstodo"])
