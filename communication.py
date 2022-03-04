b7e391211914248438a4efcba1fe45a3b51d683f
csc1028Tester <100857038+csc1028Tester@users.noreply.github.com>
2022-03-04 20:24:51 +0000

    added push notfications

--- communication.py
+++ communication.py
@@ -1,18 +1,27 @@
 import smtplib
+from pushbullet import Pushbullet
 
 
 ## The Communication Library of the project. Run through a sccdhuledTask or when called through the code.
 
 
+def send_push_notification(message):
+    api_key = "o.UINgqIWYaGd9m0s43C3W0CjOinoaANzu"
+
+    push_bullet = Pushbullet(api_key)
+    push = push_bullet.push_note("Notification from the Human Program", message)
+
+
 class Communication:
-    def __init__(self, sent_from, password,url):
+    def __init__(self, sent_from, password, url):
         self.sentFrom = sent_from
         self.password = password
         self.emailSmtp = 'smtp.gmail.com'
         self.portNumber = 465
         self.url = url
 
-    def send_email(self, email_address, person_name, task_name, uuid): # the method that sends emails as describied in What has been done so far with Communication Library
+    def send_email(self, email_address, person_name, task_name,
+                   uuid):  # the method that sends emails as describied in What has been done so far with Communication Library
         to = email_address
         subject = 'The task has been finished'
         body = 'Dear ' + person_name + '.\n We can herby the task called ', task_name + "should be finished. Please Reply, King Regards, The system. " + self.url + "/respond" + uuid + "/go"
